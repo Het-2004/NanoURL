d9cfc16f37eaf61e49ae2ff6a18b332c43bf33ab
Het-2004 <solankihetpravinbhai2122004@gmail.com>
2026-01-12 22:55:08 +0530

    chore: update .gitignore and add Frontend (Vite React) scaffold; add Backend sources

---
+++ Backend/src/main/java/com/example/NanoURL/NanoUrlApplication.java
@@ -0,0 +1,13 @@
+package com.example.NanoURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoUrlApplication.class, args);
+	}
+
+}
